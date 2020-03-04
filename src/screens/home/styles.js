fe053f0fa5aea77df723c22c9d3bdecc9f37b971
Redeemer40 <victorjonah199@gmail.com>
2020-03-05 00:07:15 +0100

    ...

--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -2,6 +2,8 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: "red"
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
